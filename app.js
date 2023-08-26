90937fe7f870eb74b36e16b3c705e4f7b9878acd
maxterjunior <vecacriju@gmail.com>
2023-08-25 22:12:42 -0500

    fix body pasrse

--- app.js
+++ app.js
@@ -11,128 +11,138 @@ const logMemoryUsage = (id, message) => {
   return msg
 }
 
+app.use(express.json());
+
+
 app.post('/', async (req, res) => {
 
-  const { width = 540, height = 540 } = req.body
+  try {
 
-  const { polygons = [] } = req.body
+    const { width = 540, height = 540 } = req.body
 
-  if (polygons?.length === 0) res.status(400).json({
-    ok: false,
-    statusCode: 400,
-    message: 'No se recibieron polígonos',
-  })
+    const { polygons = [] } = req.body
 
-  const polygonFeatures = polygons.map((polygon) => {
-    const properties = { ...polygon }
-    delete properties.coordinates
-    return {
-      "type": "Feature",
-      "properties": properties,
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": polygon.coordinates || []
+    if (polygons?.length === 0) res.status(400).json({
+      ok: false,
+      statusCode: 400,
+      message: 'No se recibieron polígonos',
+    })
+
+    const polygonFeatures = polygons.map((polygon) => {
+      const properties = { ...polygon }
+      delete properties.coordinates
+      return {
+        "type": "Feature",
+        "properties": properties,
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": polygon.coordinates || []
+        }
       }
-    }
-  })
+    })
 
-  const browser = await puppeteer.launch({ width, height, headless: true });
-  const page = await browser.newPage();
-  logMemoryUsage('1', 'Antes de cargar el mapa')
-  // Crear un mapa Leaflet en HTML
-  const html = `
-    <html>
-
-    <head>
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-      <style>
-        body {
-          margin: 0;
-        }
-    
-        #map {
-          width: 100vw;
-          height: 100vh;
-        }
-
-        .label {
-          display:flex;
-          text-align:center;
-        }
-
-        .footer {
-          position: fixed;
-          bottom: 0px;
-          right: 0px;
-          font-size: 12px;
-          z-index: 1001;
-          color: dodgerblue;
-          background: white;
-          padding:3px;
-        }
-      </style>
-    </head>
-    
-    <body>
-      <div class="footer">Yawi - Mj</div>
-      <div id="map"></div>
-      <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-      <script>
-        const map = L.map('map', { fadeAnimation: false, zoomAnimation: false }).setView([0, 0], 2);
-        L.tileLayer('http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}', { maxZoom: 20, subdomains: ["mt0", "mt1", "mt2", "mt3"], }).addTo(map);
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',{ maxZoom: 17 }).addTo(map);
-        
-        // Remover controles innecesarios
-        map.zoomControl.remove();
-
-        // Generar polígonos aleatorios 
-        var geojsonFeature = ${JSON.stringify(polygonFeatures)};
-    
-        // Dibujar polígonos en el mapa
-        const drawnPolygons = L.geoJSON(geojsonFeature, {
-          onEachFeature:  (feature, layer) => {
-
-            layer.setStyle({
-              color: feature.properties.border || '#000',
-              fillColor: feature.properties.fill || '#000',
-              fillOpacity: feature.properties.fillOpacity || 0.2,
-              opacity: feature.properties.opacity || 1,
-            })
-
-            const label = L.marker(layer.getBounds().getCenter(), {
-              icon: L.divIcon({
-                className: 'label',
-                html: feature.properties.label,
-                iconSize: [feature.properties.label.length * 6.5, 20]
+    const browser = await puppeteer.launch({ width, height, headless: true });
+    const page = await browser.newPage();
+    logMemoryUsage('1', 'Antes de cargar el mapa')
+    // Crear un mapa Leaflet en HTML
+    const html = `
+      <html>
+  
+      <head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+        <style>
+          body {
+            margin: 0;
+          }
+      
+          #map {
+            width: 100vw;
+            height: 100vh;
+          }
+  
+          .label {
+            display:flex;
+            text-align:center;
+          }
+  
+          .footer {
+            position: fixed;
+            bottom: 0px;
+            right: 0px;
+            font-size: 12px;
+            z-index: 1001;
+            color: dodgerblue;
+            background: white;
+            padding:3px;
+          }
+        </style>
+      </head>
+      
+      <body>
+        <div class="footer">https://github.com/maxterjunior</div>
+        <div id="map"></div>
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+        <script>
+          const map = L.map('map', { fadeAnimation: false, zoomAnimation: false }).setView([0, 0], 2);
+          L.tileLayer('http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}', { maxZoom: 20, subdomains: ["mt0", "mt1", "mt2", "mt3"], }).addTo(map);
+          L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',{ maxZoom: 17 }).addTo(map);
+          
+          // Remover controles innecesarios
+          map.zoomControl.remove();
+  
+          // Generar polígonos aleatorios 
+          var geojsonFeature = ${JSON.stringify(polygonFeatures)};
+      
+          // Dibujar polígonos en el mapa
+          const drawnPolygons = L.geoJSON(geojsonFeature, {
+            onEachFeature:  (feature, layer) => {
+  
+              layer.setStyle({
+                color: feature.properties.border || '#000',
+                fillColor: feature.properties.fill || '#000',
+                fillOpacity: feature.properties.fillOpacity || 0.2,
+                opacity: feature.properties.opacity || 1,
               })
-            }).addTo(map);
-          },
-        }).addTo(map);
-    
-        // Ajustar el zoom del mapa para que se vean todos los polígonos
-        map.fitBounds(drawnPolygons.getBounds());
-    
-      </script>
-    </body>
-    
-    </html>
-  `;
+  
+              const label = L.marker(layer.getBounds().getCenter(), {
+                icon: L.divIcon({
+                  className: 'label',
+                  html: feature.properties.label,
+                  iconSize: [feature.properties.label.length * 6.5, 20]
+                })
+              }).addTo(map);
+            },
+          }).addTo(map);
+      
+          // Ajustar el zoom del mapa para que se vean todos los polígonos
+          map.fitBounds(drawnPolygons.getBounds());
+      
+        </script>
+      </body>
+      
+      </html>
+    `;
 
-  await page.setContent(html);
-  await page.waitForSelector('#map > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-tile-pane > div.leaflet-layer:nth-child(1) > div:nth-child(1) > img')
+    await page.setContent(html);
+    await page.waitForSelector('#map > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-tile-pane > div.leaflet-layer:nth-child(1) > div:nth-child(1) > img')
 
-  logMemoryUsage('2', 'Después de cargar el mapa')
-  const screenshot = await page.screenshot();
+    logMemoryUsage('2', 'Después de cargar el mapa')
+    const screenshot = await page.screenshot();
 
-  await browser.close();
+    await browser.close();
 
-  logMemoryUsage('3', 'Después de cerrar el navegador')
-  // Devolver la imagen como respuesta
-  res.writeHead(200, {
-    'Content-Type': 'image/png',
-    'Content-Length': screenshot.length,
-  });
-  res.end(screenshot);
+    logMemoryUsage('3', 'Después de cerrar el navegador')
+    // Devolver la imagen como respuesta
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': screenshot.length,
+    });
+    res.end(screenshot);
+  } catch (e) {
+    console.log(e)
+    res.status(500).send({ error: e.message })
+  }
+
 });
 
 app.get('/', (req, res) => {
